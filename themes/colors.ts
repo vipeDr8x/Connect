eacfb2288d39a3ae15bbf74099b2707bdf131279
Zdravko Ayvazov <ajvazovzdravko@gmail.com>
2026-06-11 12:23:22 +0300

    Add copyright tag

--- themes/colors.ts
+++ themes/colors.ts
@@ -1,16 +1,17 @@
 const colors = {
-  steelBlue: "#352117",
-  warmColor: "#E8C89A",
-  warmCream: "#E2D5BC",
+  brown: "#352117",
+  warmColor: "#7d694e",
+  warmCream: "#F2EBE0",
   white: "#FFFFFF",
   black: "#0000",
+  steelBlue: "#5B8FA8",
 };
 
 export const theme = {
   primary: colors.warmCream,
   mainTextColor: colors.black,
   surface: colors.white,
-  brand: colors.steelBlue,
+  brand: colors.brown,
   accent: colors.warmColor,
   invertedTextColor: colors.white,
 };
